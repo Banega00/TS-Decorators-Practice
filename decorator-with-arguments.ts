c266f6e6fea972e51f0f212f5599fe8de9f10632
Banega00 <bane.stojanovic00@gmail.com>
2022-07-25 15:11:01 +0200

    decorators with arguments

---
+++ decorator-with-arguments.ts
@@ -0,0 +1,24 @@
+/*in order to pass additional arguments to a decorator
+you have to use Decorator Factory
+
+Decorator Factory is function that receives parameters and 
+returns a Function with a Decorator Signature
+*/
+
+const DecoratorWithParams = (a: string, b?: number) => {
+
+    //the original decorator
+    return function(target: Function & typeof TestClass): void{
+        target.prototype.id = a;
+        target.prototype.b = b;
+    }
+}
+
+@DecoratorWithParams('testID',3)
+class TestClass{
+    id: string;
+    b: number;
+}
+
+const testClass = new TestClass()
+console.log(testClass.id, testClass.b)
